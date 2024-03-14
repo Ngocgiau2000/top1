ba0defe67093933f90e00ab1519e586a9fbdf393
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-03-14 12:36:47 +0700

    Add files via upload

---
+++ alll.sh
@@ -0,0 +1,265 @@
+#!/bin/sh
+xanh1='\033[34;1m'
+xanh2='\033[32;1m'
+hong='\033[35;1m'
+luc='\033[36;1m'
+d='\033[31;1m'
+trang='\033[37;1m'
+vang='\033[33;1m'
+echo
+echo
+echo
+echo $xanh1"["$vang"•"$xanh1"]"$xanh2"───────────────────────────────────────────"$xanh1"["$vang"•"$xanh1"]"
+echo $xanh2" |"$d"      我爱中国 tool v1.5 pro 我爱中国        "$xanh2"|"
+echo $xanh2" |"$xanh2"─────────────────────────────────────────────"$xanh2"|"
+echo $xanh2" |"$d"["$xanh2"admin"$d"]"$vang":"$trang" dien-dev   [base64]"   $vang"100"$d"℅"$xanh2"            |"
+echo $xanh2" |"$d"["$xanh2"FACEBOOK"$d"]"$vang":"$trang" dien-dev.code64"    $vang"100"$d"℅"$xanh2"             |"
+echo $xanh2" |"$d"["$xanh2"Menu"$d"]"$vang":"$trang"  [Auto] [adb] [auto dao coin]" $vang"100"$d"℅"$xanh2"   |"
+echo $xanh2" |"$d"["$xanh2"CPU"$d"]"$vang":"$trang"  [boot cpu ] [max] [hashrate]" $vang"100"$d"℅"$xanh2"    |"
+echo $xanh2" |"$d"["$xanh2"Lien He"$d"]"$vang":"$trang" 0899717143" $vang"100"$d"℅"$xanh2"                   |"
+echo $xanh2" |"$d"["$xanh2"WARNIING"$d"]"$vang":"$trang" NO DDOS NO VIRUS NO COOCKIES" $vang"100"$d"℅"$xanh2"|"
+echo $xanh1"["$vang"•"$xanh1"]"$xanh2"───────────────────────────────────────────"$xanh1"["$vang"•"$xanh1"]"
+echo
+echo $xanh2"─────────────────────────────────────"
+echo $hong"◉‿◉"$luc" 𝟏"$hong"👉"$trang" 🄼ining verus termux]        "$hong"∆"
+echo $hong"◉‿◉"$luc" 𝟐"$hong"👉"$trang" 🄼ining verus ubuntu]        "$hong"∆"
+echo $hong"◉‿◉"$luc" 𝟯"$hong"👉"$trang" 🄼ining xmrig termux         "$hong"∆"
+echo $hong"◉‿◉"$luc" 𝟒"$hong"👉"$trang" 🄼ining xmrig ubuntu         "$hong"∆"
+echo $hong"◉‿◉"$luc" 𝟓"$hong"👉"$trang" 🄼ining dero                 "$hong"∆"
+echo $hong"◉‿◉"$luc" 𝟲"$hong"👉"$trang" 🄼ining cpuminer otp termux  "$hong"∆"
+echo $hong"◉‿◉"$luc" 𝟕"$hong"👉"$trang" 🄼ining cpuminer otp ubuntu  "$hong"∆"
+echo $hong"◉‿◉"$luc" 𝟖"$hong"👉"$trang" 🄱uff view tiktok            "$hong"∆"
+echo $hing"◉‿◉"$luc" 𝟗"$hong"👉"$trang" 🄲laim coin faucetpay        "$hong"∆"
+echo $hong"◉‿◉"$luc"10"$hong"👉"$trang" 🄼ining cpuminer multi termux"$hong"∆"
+echo $hong"◉‿◉"$luc"11"$hong"👉"$trang" 🄼ining cpuminer multi ubuntu"$hong"∆"
+echo $hong"◉‿◉"$luc"12"$hong"👉"$trang" 🄼ining M7M hash termux      "$hong"∆"
+echo $hong"◉‿◉"$luc"13"$hong"👉"$trang" 🄼ining M7M hash ubuntu      "$hong"∆"
+echo $hong"◉‿◉"$luc"14"$hong"👉"$trang" 🄼ining sugar termux         "$hong"∆"
+echo $hong"◉‿◉"$luc"15"$hong"👉"$trang" 🄼ining sugar ubuntu         "$hong"∆"
+echo $xanh2"─────────────────────────────────────"
+echo $xanh2"¦"
+read -p"└──nhập-số: " stt
+if [ $stt = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt install wget -y
+apt install curl -y
+apt install openssh -y
+cd
+cd ..
+cd
+git clone https://github.com/Ngocgiau2000/devio.git && cd devio && chmod +x tool100 && ./tool100
+fi
+if [ $stt = 2 ]
+then
+clear
+figlet f slant "W AI T"|lolcat
+sleep 1
+git clone http://github.com/Ngocgiau2000/miningvip.git
+cd miningvip
+chmod +x giau
+chmod +x giau
+./giau
+fi
+if [ $stt = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt install wget -y
+apt install curl -y
+apt install openssh -y
+cd
+cd ..
+cd
+git clone https://github.com/Ngocgiau2000/devio.git && cd devio && chmod +x tool100 && ./tool100
+fi
+if [ $stt = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/dien.git
+cd dien
+chmod +x giau
+chmod +x giau
+./giau
+fi
+if [ $stt = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git  clone https://github.com/Ngocgiau2000/derovip.git
+cd derovip
+chmod +x dero2
+chmod +x dero2
+./dero2
+fi
+if [ $stt = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt install wget -y
+apt install curl -y
+apt install openssh -y
+cd
+cd ..
+cd
+git clone https://github.com/Ngocgiau2000/devio.git && cd devio && chmod +x tool100 && ./tool100
+fi
+if [ $stt = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/cpuminerall.git
+cd cpuminerall
+chmod +x cpuminer2.sh
+chmod +x cpuminer2.sh
+./cpuminer2.sh
+fi
+if [ $stt = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/Tiktok.git
+cd Tiktok
+pkg install python
+pip install pystyle
+pip install requests
+python view.py
+fi
+if [ $stt = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Ngocgiau2000/claim.git
+cd claim
+php install php
+php claimvip.php
+fi
+if [ $stt = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt install wget -y
+apt install curl -y
+apt install openssh -y
+cd
+cd ..
+cd
+git clone https://github.com/Ngocgiau2000/devio.git && cd devio && chmod +x tool100 && ./tool100
+fi
+if [ $stt = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/multiall.git
+cd multiall
+chmod +x multi
+chmod +x multi
+./multi
+fi
+if [ $stt = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt install wget -y
+apt install curl -y
+apt install openssh -y
+cd
+cd ..
+cd
+git clone https://github.com/Ngocgiau2000/devio.git && cd devio && chmod +x tool100 && ./tool100
+fi
+if [ $stt = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/m7m.git
+cd m7m
+chmod +x m7m
+chmod +x m7m
+./m7m
+fi
+if [ $stt = 14 ]
+then
+clear
+apt install wget -y
+apt install curl -y
+apt install openssh -y
+cd
+cd ..
+cd
+git clone https://github.com/Ngocgiau2000/devio.git && cd devio && chmod +x tool100 && ./tool100
+fi
+if [ $stt = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/suga.git
+cd suga
+chmod +x suga
+chmod +x suga
+./suga
+fi
+if [ $stt = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+if [ $stt = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+chmod +x *
+python2 weeman.py
+fi
+if [ $stt = 18 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+if [ $stt = 19 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $pur">> Thank You ban quen admin dien base64 <<"
+exit
